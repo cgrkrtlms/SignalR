787eb529f6f334533609f9a893d457dfd47949de
İbrahim Çağrı Kurtulmuş <cagrikurtulmus@gmail.com>
2022-01-19 14:36:27 +0300

    Add project files.

---
+++ SignalR.API/Hubs/MyHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.API.Hubs
+{
+    public class MyHub:Hub
+    {
+        // Clientlar SendMessage methoduna istek yapacaklar ve name isminde bir data gönderecekler.Daha sonra bu method çalıştığı zaman Clientlar üzerindeki "ReceiveMessage" a bir bildiri gidecek.(Clientlar üzerindeki ReceiveMessage methodu çalışsın, çalışırken name parametresinde gönderilen data da gitsin.).Eğer Clientlar "ReceiveMessage" methoduna subcribe olmuşsa method çalışacak,eğer subcribe olmamışsa mesajları server'dan almayacak. 
+
+        public static List<string> Names { get; set; } = new List<string>();
+
+        public async Task SendName(string name)
+        {
+            Names.Add(name);
+
+            // All : Bu Hub'a bağlı olan tüm clientlara bildiri gönderir.Clientlardaki şu method çalışsın diye.
+            await Clients.All.SendAsync("ReceiveName", name);
+        }
+
+        public async Task GetNames()
+        {
+            await Clients.All.SendAsync("ReceiveNames",Names);
+        }
+
+    }
+}
